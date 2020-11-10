88967cf1f4d4b804bc46b138829085cd587c4833
chaoticgd <chaoticgd@protonmail.com>
2020-11-10 00:00:45 +0000

    Add simple random C program generator
    In the future I want to use existing solutions instead, but
    since we currently don't support function calls this works for now.

---
+++ misc/generate_c.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main();
+void gen_statement();
+void gen_block();
+void gen_expr();
+
+int locals;
+int depth = 0;
+int iterator_count = 0;
+int last_assigned = 0;
+
+int main()
+{
+	srand(time(NULL));
+	
+	printf("#ifdef __x86_64__\n");
+	printf("int main() {\n");
+	printf("#else\n");
+	printf("int _start() {\n");
+	printf("#endif\n");
+	locals = 1 + rand() % 32;
+	for(int i = 0; i < locals; i++) {
+		printf("int l_%d = %d;\n", i, rand());
+	}
+	for(int i = 0; i < 10; i++) {
+		gen_statement();
+	}
+	printf("return l_%d;\n", last_assigned);
+	printf("}\n");
+	return 0;
+}
+
+void gen_statement()
+{
+	depth++;
+	switch(rand() % 2) {
+		case 0: {
+			if(depth < 3) {
+				gen_block();
+				break;
+			}
+		}
+		case 1: { // assignment
+			last_assigned = rand() % locals;
+			printf("l_%d = ", last_assigned);
+			gen_expr();
+			printf(";\n");
+			break;
+		}
+	}
+	depth--;
+}
+
+void gen_block()
+{
+	depth++;
+	switch(rand() % 2) {
+		case 0: {
+			printf("for(int i_%d = 0; i_%d < %d; i_%d++)",
+				iterator_count, iterator_count, rand() % 1024, iterator_count);
+			iterator_count++;
+			break;
+		}
+		case 1: {
+			printf("if(");
+			gen_expr();
+			printf(")\n");
+			break;
+		}
+	}
+	printf(" {\n");
+	for(int i = 0; i < 3 - depth; i++) {
+		gen_statement();
+	}
+	printf("}\n");
+	depth--;
+}
+
+void gen_expr()
+{
+	depth++;
+	if(depth > 5) {
+		if(rand() % 2 == 0) {
+			printf("l_%d", rand() % locals);
+		} else {
+			printf("%d", rand());
+		}
+	} else {
+		switch(rand() % 3) {
+			case 0: {
+				printf("(");
+				gen_expr();
+				printf(")+(");
+				gen_expr();
+				printf(")");
+				break;
+			}
+			case 1: {
+				printf("(");
+				gen_expr();
+				printf(")-(");
+				gen_expr();
+				printf(")");
+				break;
+			}
+			case 2: {
+				printf("(");
+				gen_expr();
+				printf(")*(");
+				gen_expr();
+				printf(")");
+				break;
+			}
+		}
+	}
+	depth--;
+}
---
+++ misc/generate_c.sh
@@ -0,0 +1,12 @@
+misc_dir=$(dirname $0)
+echo "${misc_dir}"
+gcc "${misc_dir}/generate_c.c" -o "${misc_dir}/generate_c"
+${misc_dir}/generate_c > /tmp/quadra_genc.c
+gcc /tmp/quadra_genc.c -o /tmp/quadra_genc_native
+/tmp/quadra_genc_native
+target_code=$(echo $?)
+ee-gcc /tmp/quadra_genc.c -o /tmp/quadra_genc_mips -nostdlib -w -std=c99 -O3
+${misc_dir}/../quadra /tmp/quadra_genc_mips > /tmp/quadra_genc_llvm.ll
+clang /tmp/quadra_genc_llvm.ll -o /tmp/quadra_genc_translated
+/tmp/quadra_genc_translated
+echo "exit code: $?, target code: ${target_code}"
