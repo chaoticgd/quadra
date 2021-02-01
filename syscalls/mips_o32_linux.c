dd6ac7848977aef24a71052b525b626ebe8e353f
chaoticgd <chaoticgd@protonmail.com>
2021-02-01 01:04:43 +0000

    Add printfs to the syscall library

--- syscalls/mips_o32_linux.c
+++ syscalls/mips_o32_linux.c
@@ -3,25 +3,30 @@
 
 unsigned int qsys_exit(unsigned int error_code)
 {
+	printf("exit(%d)\n", error_code);
 	exit(error_code);
 }
 
 unsigned int qsys_read(unsigned int fd, char* buf, size_t count)
 {
+	printf("read(%d, %p, %ld)\n", fd, buf, count);
 	read(fd, buf, count);
 }
 
 unsigned int qsys_write(unsigned int fd, const char* buf, size_t count)
 {
+	printf("write(%d, %p, %ld)\n", fd, buf, count);
 	write(fd, buf, count);
 }
 
 unsigned int qsys_open(const char* filename, int flags, unsigned short mode)
 {
+	printf("open(%p, %d, %d)\n", filename, flags, mode);
 	open(filename, flags, mode);
 }
 
 unsigned int qsys_close(unsigned int fd)
 {
+	printf("close(%d)\n", fd);
 	close(fd);
 }
