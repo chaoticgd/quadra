b78d5de72daf7b03302e6a665ea7f1e6e5b5e604
chaoticgd <chaoticgd@protonmail.com>
2021-02-11 08:24:02 +0000

    Make the genc test cases link against the syscall library

--- misc/generate_c.sh
+++ misc/generate_c.sh
@@ -6,7 +6,7 @@ gcc /tmp/quadra_genc.c -o /tmp/quadra_genc_native
 target_code=$(echo $?)
 ee-gcc /tmp/quadra_genc.c -o /tmp/quadra_genc_mips -nostdlib -w -std=c99 -O3
 ${misc_dir}/../quadra /tmp/quadra_genc_mips > /tmp/quadra_genc_llvm.ll
-clang /tmp/quadra_genc_llvm.ll -o /tmp/quadra_genc_translated
+clang /tmp/quadra_genc_llvm.ll "${misc_dir}/../libmips_o32_linux.a" -o /tmp/quadra_genc_translated
 /tmp/quadra_genc_translated
 exit_code=$?
 echo "exit code: ${exit_code}, target code: ${target_code}"
