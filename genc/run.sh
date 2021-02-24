312fb1c266cada814baab599f7691775cdb1c074
chaoticgd <chaoticgd@protonmail.com>
2021-02-24 21:40:23 +0000

    testing: Add support for ldrgen

--- genc/run.sh
+++ genc/run.sh
@@ -1,6 +1,25 @@
 genc_dir=$(dirname $0)
-gcc "${genc_dir}/generate_c.c" -o "${genc_dir}/generate_c"
-${genc_dir}/generate_c $@ > /tmp/quadra_genc.c
+if [[ -z "${USE_LDRGEN}" ]]; then
+	gcc "${genc_dir}/generate_c.c" -o "${genc_dir}/generate_c"
+	${genc_dir}/generate_c $@ > /tmp/quadra_genc.c
+else
+	# HACK: Keep generating functions until we get one with the right prototype.
+	src=""
+	while [[ ${src} != *"int fn1(int p)"* ]]; do
+		src=$(frama-c -ldrgen -ldrgen-int-only -ldrgen-max-args 1 -ldrgen-no-div-mod -ldrgen-no-pointer-args)
+	done
+	echo -e \
+		"#pragma GCC diagnostic ignored \"-Woverflow\"\n" \
+		"${src}\n" \
+		"#ifdef __x86_64__\n" \
+		"int main() {\n" \
+		"#else\n" \
+		"int _start() {\n" \
+		"#endif\n" \
+		"return fn1(0);\n" \
+		"}\n" \
+		> /tmp/quadra_genc.c
+fi
 gcc /tmp/quadra_genc.c -o /tmp/quadra_genc_native
 /tmp/quadra_genc_native
 target_code=$(echo $?)
