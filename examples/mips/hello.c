773c365ed7320d62dcbbb920913e1e990e7c0f92
chaoticgd <chaoticgd@protonmail.com>
2021-03-04 04:20:57 +0000

    Simplify MIPS hello world example

--- examples/mips/hello.c
+++ examples/mips/hello.c
@@ -21,24 +21,11 @@ u32 syscall(u32 num, u32 arg0, u32 arg1, u32 arg2)
 	return retval;
 }
 
-// Stack strings.
-#define SS(o, c1) o[i++] = c1;
-#define SS4(o, c1, c2, c3, c4) SS(o, c1); SS(o, c2); SS(o, c3); SS(o, c4);
-#define SS12(o, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12) \
-	i = 0; \
-	SS4(o, c1, c2, c3, c4); \
-	SS4(o, c5, c6, c7, c8); \
-	SS4(o, c9, c10, c11, c12);
-
-#define STDOUT
-#define O_WRONLY	00000001
-
 void _start()
 {
-	char path[12];
-	int i;
-	
-	char greeting[12];
-	SS12(greeting, 'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!');
-	syscall(SYS_write, 1, (u32) greeting, 12);
+	char str[13];
+	str[0] = 'H'; str[1] = 'e'; str[2] = 'l'; str[3] = 'l'; str[4] = 'o';
+	str[5] = ' '; str[6] = 'w'; str[7] = 'o'; str[8] = 'r'; str[9] = 'l';
+	str[10] = 'd'; str[11] = '!'; str[12] = '\n';
+	syscall(SYS_write, 1, (u32) str, 13);
 }
