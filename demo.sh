042ce33b4e4d263c93b9b94f523557dcf7dffdb9
chaoticgd <chaoticgd@protonmail.com>
2021-03-04 02:06:34 +0000

    Add MIPS and x86 assembly add examples to demo

--- demo.sh
+++ demo.sh
@@ -5,12 +5,14 @@ heading() {
 }
 
 run_demo() {
-	heading "Source code"
-	cat "$1"
-	heading "MIPS Disassembly"
-	ee-objdump -D "$2"
+	if [[ "$2" != "" ]]; then # Skip printing out the source code for hand assembly.
+		heading "Source code"
+		cat "$2"
+	fi
+	heading "Disassembly"
+	"$1" -D "$3"
 	heading "P-Code IR"
-	./quadra "$2" > /tmp/prog.ll
+	./quadra "$3" > /tmp/prog.ll
 	heading "Compiling and running"
 	clang++ /tmp/prog.ll libmips_o32_linux.a -o /tmp/prog
 	/tmp/prog
@@ -19,7 +21,11 @@ run_demo() {
 
 # A number of pre-prepared example programs.
 case "$1" in
-	("add") run_demo examples/addc.c examples/mips/addc;;
-	("loop") run_demo examples/loop.c examples/mips/loop;;
-	("intargs") run_demo examples/intargs.c examples/mips/intargs;;
+	# MIPS examples
+	("add")     run_demo ee-objdump "" examples/mips/add;;
+	("addc")    run_demo ee-objdump examples/addc.c examples/mips/addc;;
+	("loop")    run_demo ee-objdump examples/loop.c examples/mips/loop;;
+	("intargs") run_demo ee-objdump examples/intargs.c examples/mips/intargs;;
+	# x86 examples
+	("x86add")  run_demo    objdump "" examples/x86/add;;
 esac
