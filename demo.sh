9d4fe4827458efeba12c00692b7b6d1eb4d4c110
chaoticgd <chaoticgd@protonmail.com>
2021-04-27 01:46:22 +0100

    Modify demo.sh so MIPS toolchain isn't required

--- demo.sh
+++ demo.sh
@@ -10,7 +10,10 @@ run_demo() {
 		cat "$2"
 	fi
 	heading "Disassembly"
-	"$1" -D "$3"
+	# NOTE: This demo.sh file has been modified since it was used in the
+	# presentation such that you don't need a MIPS toolchain installed in order
+	# to run it.
+	cat "$1"
 	heading "P-Code IR"
 	./quadra "$3" > /tmp/prog.ll
 	heading "Compiling and running"
@@ -22,10 +25,10 @@ run_demo() {
 # A number of pre-prepared example programs.
 case "$1" in
 	# MIPS examples
-	("add")     run_demo ee-objdump "" examples/mips/add;;
-	("loop")    run_demo ee-objdump examples/loop.c examples/mips/loop;;
-	("intargs") run_demo ee-objdump examples/intargs.c examples/mips/intargs;;
-	("hello")   run_demo ee-objdump examples/mips/hello.c examples/mips/hello;;
+	("add")     run_demo dis/mips_add.txt "" examples/mips/add;;
+	("loop")    run_demo dis/mips_loop.txt examples/loop.c examples/mips/loop;;
+	("intargs") run_demo dis/mips_intargs.txt examples/intargs.c examples/mips/intargs;;
+	("hello")   run_demo dis/mips_hello.txt examples/mips/hello.c examples/mips/hello;;
 	# x86 examples
-	("x86add")  run_demo    objdump "" examples/x86/add;;
+	("x86add")  run_demo    dis/x86_add.txt "" examples/x86/add;;
 esac
